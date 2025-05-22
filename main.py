48332afb22e399315939a963bc9ff9f0d7713491
Douglas Wiercinski <dougwier@talvie.com>
2025-05-22 15:14:35 -0700

    initial commit, pygame starts a black screen than can be closed

---
+++ main.py
@@ -0,0 +1,28 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    x = 0
+    while x == 0:
+        black = (0,0,0)
+        pygame.Surface.fill(screen, black, rect=None, special_flags=0)
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        
+
+if __name__ == "__main__":
+    main()
